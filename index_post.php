e47b8312be940f0d9bf37f632dc00772fb485c79
raphaeldebernardis <raphael2bernardis@gmail.com>
2014-10-29 14:28:09 +0100

    Create index_post.php

---
+++ index_post.php
@@ -0,0 +1,17 @@
+<?php header('Access-Control-Allow-Origin: *'); ?> //on autorise l'accès à cet affichage
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=edt', 'root', ''); //connexion à la base de données
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage()); //erreur si on n'arrive pas à se connecter
+}
+
+$reponse = $bdd->query('SELECT * FROM matiere'); //preparation et exécution de la requête pour afficher toutes les matières
+
+$result = $reponse->fetchAll(PDO::FETCH_ASSOC); //on met tous les résultats dans $result à l'aide d'un curseur
+$reponse->closeCursor();                        //on ferme le curseur
+
+echo json_encode($result, JSON_PRETTY_PRINT);   //on retourne le résultat dans un tableau JSON
